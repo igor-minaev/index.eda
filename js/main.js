d78d22fe975276205185c7f5d7d5e6770d62f25d
igor-minaev <igor.minayeu@gmail.com>
2021-10-26 21:36:53 +0300

    responsive

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    prevEl: '.slider-button-prev',
+    nextEl: '.slider-button-next',
+  },
+});
